901f8e3f956baca1e7d3e0ea98c55ce8b7ba347b
Alejandro Huante <alejandrohuante1@gmail.com>
2020-05-30 15:02:20 -0700

    Started JS file

---
+++ tours-app/src/Sightseeing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './indexStyle.css';
+import './style.css';
+
+class Sightseeing extends Component {
+
+    constructor() {
+      super();
+      // empty states
+      this.state = {
+        sortOrder: 'default',
+        filter: 'default',
+      }
+    }
+}
