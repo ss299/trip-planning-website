be05ea13d96727a3d757f6ece054131def260921
Alejandro Huante <alejandrohuante1@gmail.com>
2020-05-30 17:18:07 -0700

    Added cards and country filter

---
+++ tours-app/src/sightseeingCards.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'; //import React Component
+import {
+    Card, Button
+} from 'reactstrap-bootstrap/Card';
+import './indexStyle.css';
+import './style.css';
+
+export class sightseeingCards extends Component {
+    render() {
+        
+        let event = this.props.event;
+
+        return (
+            <Card style={{ width: '20rem' }}>
+                <Card.Img variant="top" src="event.image" />
+                {/* card rank? */}
+                <Card.Body>
+                    <Card.Title>{event.name} , {event.country}</Card.Title>
+                    <Card.Text>{event.description}</Card.Text>
+                    <Button variant="primary" href="#">More details</Button>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
+
+export default sightseeingCards;
