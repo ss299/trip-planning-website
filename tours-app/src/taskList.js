43a6a6c4aa352581c083bbbebe93b015d5c74ef3
grtyang <gryang@uw.edu>
2020-05-31 15:56:39 -0700

    fix

--- tours-app/src/taskList.js
+++ tours-app/src/taskList.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './indexStyle.css';
 import './style.css';
-import { Card, Button, Jumbotron, Container } from 'react-bootstrap';//import React Component
-import HomePage from './HomePage'
+import './newTrip.js';
 
 class TaskList extends Component {
 
     changeState = () => {
         let modalHidden = "modal modal-hidden"
-        if (this.props.state == true) {
+        if (this.props.state === true) {
             modalHidden = "modal"
         }
         console.log(modalHidden)
@@ -17,10 +16,12 @@ class TaskList extends Component {
 
     render() {
         return(
+            <div>
         <div className="taskManager container col"></div>
         <input className="newTask" type="text" placeholder="add a new task"/>
         <h3>NotCompleted</h3>
         <div className ="notcomp"></div>
+            </div>
         )
     }
 }
