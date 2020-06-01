67bcbb7964620a4b82310cd8a85c182de4c00095
grtyang <gryang@uw.edu>
2020-05-31 19:51:14 -0700

    Todolist

--- tours-app/src/taskList.js
+++ tours-app/src/taskList.js
@@ -2,52 +2,39 @@ import React, { Component } from 'react';
 import './indexStyle.css';
 import './style.css';
 import './newTrip.js';
-import Tasklist from '@humanmade/react-tasklist';
-import '@humanmade/react-tasklist/css/index.css';
+import List from './List';
 
-export class TaskList extends Component {
-    constructor( props ) {
-        super( props );
-        this.state = {
-			items: [
-				{
-					label: 'Item one',
-					checked: false,
-				},
-				{
-					label: 'Item two',
-					checked: false,
-					disabled: true,
-				},
-				{
-					label: 'Item three',
-					checked: true,
-				},
-			],
-		};
-	}
-
-	onChange = ( index, checked ) => {
-		const { items } = this.state;
-		this.setState( {
-			items: [
-				...items.slice( 0, index ),
-				{ ...items[ index ], checked },
-				...items.slice( index + 1 )
-			],
-		} );
-	}
-
-	render() {
-		const { items } = this.state;
-
-		return <Tasklist
-			items={ items }
-			onChange={ this.onChange }
-			onReorder={ items => this.setState( { items } ) }
-		/>
+export default class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        term: '',
+        items: []
+      };
     }
-}
-
-export default TaskList;
-
+  
+    onChange = (event) => {
+      this.setState({ term: event.target.value });
+    }
+  
+    onSubmit = (event) => {
+      event.preventDefault();
+      this.setState({
+        term: '',
+        items: [...this.state.items, this.state.term]
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <form className="App" onSubmit={this.onSubmit}>
+            <input value={this.state.term} onChange={this.onChange} />
+            <button>Add todos</button>
+          </form>
+          <h2>NotCompleted</h2>
+          <List items={this.state.items} />
+        </div>
+      );
+    }
+  }
